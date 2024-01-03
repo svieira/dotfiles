0a504968bc04b6df69bea8ea22ea8dfaf094a665
Sean Vieira <sean.vieira@appian.com>
2024-01-03 00:49:01 -0500

    Update brew prefix to work across Intel and ARM Macs

--- fzf/fzf.bash
+++ fzf/fzf.bash
@@ -1,13 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/usr/local/opt/fzf/bin"
+if [[ ! "$PATH" == *$PACKAGE_MANAGER_PREFIX/opt/fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}$PACKAGE_MANAGER_PREFIX/opt/fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.bash" 2> /dev/null
+[[ $- == *i* ]] && source "$PACKAGE_MANAGER_PREFIX/opt/fzf/shell/completion.bash" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/usr/local/opt/fzf/shell/key-bindings.bash"
+source "$PACKAGE_MANAGER_PREFIX/opt/fzf/shell/key-bindings.bash"
